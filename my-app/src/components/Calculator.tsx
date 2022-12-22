47664f593e1e61c725af4b7ec9c36bdeeac47332
youva <kadji_y@etna-alternance.net>
2022-12-22 15:18:08 +0100

    first

---
+++ my-app/src/components/Calculator.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Container = styled.div`
+    background: #282c34;
+`;
+
+const Calculator: React.FC<{}> = () => {
+    return <Container>calculator comp</Container>
+}
+
+export default Calculator;
